871bc08e5f23654d3fe31031ed2878826d167384
AlexanderPankov1970 <pankovsasha7@gmail.com>
2022-05-30 18:02:49 +0300

    null in useState

---
+++ src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  //VAR 1
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+    //console.log(event.target.value);
+    // console.log(this.state.term);
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onFormSubmitNew(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+              //   onChange={e=>this.setState({term:e.target.value})}// VAR 2
+              placeholder="Input Text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
